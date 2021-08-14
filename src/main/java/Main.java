6f2cb6d77e3b4d8dd02f04355538ca265f2ea82a
Sven Hofmann <git@svnlib.com>
2021-08-14 16:13:02 +0200

    Updated deps and added stats

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,13 @@ import java.util.List;
 public class Main {
 
     public static void main(final String[] args) throws IOException, GitAPIException {
-        final RepoMiner miner = new RepoMiner("/Volumes/External/GitHub/git");
+        final RepoMiner miner = new RepoMiner(args[0]);
 
         final Iterable<RevCommit> commits = miner.getCommits(CommitFilter.builder().noMerges().build());
 
-        long count = 0;
+        long count            = 0;
+        long timeWas          = System.currentTimeMillis();
+        int  commitsPerSecond = 0;
         for (final RevCommit commit : commits) {
             count++;
             if (commit.getParentCount() < 1) {
@@ -22,7 +24,13 @@ public class Main {
             final List<DiffEntry> diffs = miner.getDiffsFromCommit(commit);
 
             commit.disposeBody();
-            System.out.print("\r" + count);
+            if (count % 10 == 0) {
+                final long   timeIs  = System.currentTimeMillis();
+                final double seconds = (timeIs - timeWas) / 1000.0;
+                commitsPerSecond -= (commitsPerSecond - (int) (10 / seconds)) >> 8;
+                System.out.printf("\r%.1fk commits read [%d commits/s]", count / 1000.0, commitsPerSecond);
+                timeWas = timeIs;
+            }
         }
 
         System.out.println("Done");
